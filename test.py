f3c0a9c6dd4562b8898f9a8adae2783cb6104908
BrasD99 <brasd99@gmail.com>
2023-11-19 23:03:30 +0300

    Test fix

--- test.py
+++ test.py
@@ -5,9 +5,12 @@ youtube_link = 'https://www.youtube.com/shorts/zpWWs_v2jf4'
 language = 'English'
 
 def update_progress(percent, text):
-    print(f'{percent}: {text}')
+    print(f'{percent}%: {text}')
 
-#yt = YouTube(youtube_link)
-#yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').first().download(filename='temp.mp4')
+yt = YouTube(youtube_link)
+yt.streams.filter(progressive=True, file_extension='mp4') \
+    .order_by('resolution') \
+    .first() \
+    .download(filename='temp.mp4')
 engine = Engine(language, update_progress)
 engine.process('temp.mp4', 'out.mp4')
